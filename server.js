9d13c25a77b4a84a46ae1b31b8f54d44658c2dac
Hichem Fantar <hichemfantar2049@gmail.com>
2023-04-06 22:59:33 +0100

    tutorial day 2

--- server.js
+++ server.js
@@ -5,4 +5,20 @@ app.get("/", function (req, res) {
 	res.send("Hello " + req.query.name);
 });
 
+app.get("/wie", function (req, res) {
+	res.send("Hello WIE");
+});
+
+app.get("/cs", function (req, res) {
+	res.send("Hello CS members");
+});
+
+app.get("/stagiaires", function (req, res) {
+	res.send([
+		{ name: "Eya", age: 20 },
+		{ name: "Khalil", age: 20 },
+		{ name: "Abderrazek", age: 20 },
+	]);
+});
+
 app.listen(3000);
